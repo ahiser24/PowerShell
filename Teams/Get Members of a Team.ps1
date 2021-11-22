fa9d21f6a5624a6731009de610a69222465c21d3
Andrew <65128664+ahiser24@users.noreply.github.com>
2021-11-22 14:58:24 -0500

    Add files via upload
    Type in the Team name you're looking for.
    You will be prompted for a save location for the .CSV file.
    A Grid view will display all Teams with similar names.
    CTRL+C to copy the line item from the Grid for the correct Team.
    CTRL+V into the prompt and it will pull the first 36 digits of the string which is the GroupId.
    .CSV file will be saved to the selected save location.

---
+++ Teams/Get Members of a Team.ps1
@@ -0,0 +1,39 @@
+﻿Connect-MicrosoftTeams
+$global:Team = Read-Host "What is the name of the Team?"
+
+Function ListGroupId {
+    Get-Team -DisplayName $global:Team | select -Property GroupId, DisplayName, Description | Out-GridView
+    }
+
+Function EnterGroupId {
+
+    $global:GroupID = Read-Host "Copy and Paste the GroupId of the correct Team"
+    $global:GroupID.Substring(0,36)
+    }
+
+Function Save-File ([string]$initialDirectory) {
+
+	$SaveInitialPath = "C:\"
+	$SaveFileName = "Result.csv"
+
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+    $OpenFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+    $OpenFileDialog.initialDirectory = $SaveInitialPath
+    $OpenFileDialog.filter = "CSV (*.csv)| *.csv"
+	$OpenFileDialog.FileName = $SaveFileName
+    $OpenFileDialog.ShowDialog() | Out-Null
+
+    return $OpenFileDialog.filename
+
+}
+
+$SaveMyFile = Save-File
+
+
+    ListGroupId 
+    Write-Host "Please review the Team(s) above and copy the GroupId of the correct Team."
+    EnterGroupId
+
+#Export list to CSV
+Get-TeamUser -GroupId $global:GroupID.Substring(0,36) | Export-Csv -Path $SaveMyFile
