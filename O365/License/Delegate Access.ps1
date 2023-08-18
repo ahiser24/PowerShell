f8fb0110473a289b71e03b1a9a5e2cac42ba77b8
Andrew <65128664+ahiser24@users.noreply.github.com>
2023-08-18 09:17:41 -0400

    Add files via upload

---
+++ O365/License/Delegate Access.ps1
@@ -0,0 +1,15 @@
+﻿#Connect to O365
+#Connect-MsolService
+
+#Variables
+Write-Host "Input the name of the user"
+$alias = Read-Host -Prompt 'Input User'
+Write-Host "Input the name of the delegate"
+$delegate = Read-Host -Prompt 'Input Delegate User'
+
+
+#Get Mailbox Permissions
+Get-MailboxFolder Permission "$alias":\Calendar
+
+#Add Delegate Permissions
+Add-MailboxFolderPermission "$alias":\Calendar -User $delegate -AccessRights Editor -SharingPermissionFlags Delegate
---
+++ O365/License/List of attendees.ps1
@@ -0,0 +1,24 @@
+﻿#Setup - User copies all users from the "TO" field in Outlook invite and pastes them into a txt file called names.txt.
+#The format will look like this "Hiser, Andrew - MTC <Andrew.Hiser@mosaicco.com>"
+
+#Reads the text file called names.txt from Downloads folder and converts it to a string
+$text_file = Get-Content $env:USERPROFILE\Downloads\names.txt
+#Define a string
+$text = $text_file.ToString()
+
+#Creates a regular expression to separate name and email address
+$pattern = "(?<=<).+?(?=>)"
+#Remove Symbols
+$names = $text -split $pattern -replace ";" -replace "<" -replace ">"
+$emails = $text | Select-String -Pattern $pattern -AllMatches | ForEach-Object {$_.Matches.Value}
+
+#Create columns for Name and Email
+$output = @()
+for ($i=0; $i -lt $names.Length; $i++) {
+$output += [PSCustomObject]@{
+    Name = $names[$i]
+    Email = $emails[$i]
+    }
+}
+#Exports to a CSV file in the users Downloads folder called output.csv
+$output | Export-Csv -Path $env:USERPROFILE\Downloads\output.csv -NoTypeInformation
