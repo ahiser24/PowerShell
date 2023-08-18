30b895007ccfb5b613038e7f52b48c5a89223967
Andrew <65128664+ahiser24@users.noreply.github.com>
2023-08-18 09:26:08 -0400

    Update and rename Send message to Teams WebHook.ps1 to Send message to Teams WebHook.ps1
    change folder

---
+++ Teams/Send message to Teams WebHook.ps1
@@ -0,0 +1,17 @@
+﻿#Admin SharePoint Site
+$AdminSiteURL = "https://TENANT-admin.sharepoint.com/"
+
+#Connect to SharePoint Online
+Connect-SPOService -URL $AdminSiteURL
+
+#Get all Deleted Sites
+$bodytext = Get-SPODeletedSite -Limit 1000 | Where-Object {$_.DaysRemaining -LT 10} | select "URL","DaysRemaining" | Out-String
+
+$text = $bodytext
+$uri = "WEBHOOK"
+
+$body = ConvertTo-JSON @{
+    text = $bodytext
+}
+
+Invoke-RestMethod -uri $uri -Method Post -body $body -ContentType 'application/json'
