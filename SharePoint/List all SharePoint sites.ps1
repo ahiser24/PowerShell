ac8bf2049ab25cb5175f12e2d6ae9871c6e2ba1e
Andrew <65128664+ahiser24@users.noreply.github.com>
2023-08-18 09:33:02 -0400

    Update List all SharePoint sites.ps1
    Change tenant

--- SharePoint/List all SharePoint sites.ps1
+++ SharePoint/List all SharePoint sites.ps1
@@ -1,2 +1,2 @@
-﻿Connect-SPOService -Url "https://mosaicco-admin.sharepoint.com"
+﻿Connect-SPOService -Url "https://TENANT-admin.sharepoint.com"
 Get-SPOSite -Limit "All" -IncludePersonalSite $True | Export-Csv -Path "C:\temp\SiteListing.csv"
