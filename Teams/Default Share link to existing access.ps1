585a1cdf91b8b605eac1a7a4f73e431fd99ad581
Andrew <65128664+ahiser24@users.noreply.github.com>
2023-08-18 09:41:32 -0400

    Update Default Share link to existing access.ps1
    Change tenant

--- Teams/Default Share link to existing access.ps1
+++ Teams/Default Share link to existing access.ps1
@@ -1,5 +1,5 @@
 ﻿#Connect to the admin center
-Connect-SPOService -Url "https://mosaicco-admin.sharepoint.com/"
+Connect-SPOService -Url "https://TENANT-admin.sharepoint.com/"
 
 #Set the site URL policy to Default Sharing link as Existing Access. Change SITEURL to the SharePoint URL of the Team or site.
 Set-SPOSite SITEURL -DefaultLinkToExistingAccess $true
