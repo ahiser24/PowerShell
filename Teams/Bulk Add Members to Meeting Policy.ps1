3ddfb02ae0945f415523e366796d6b7d4d4d6d09
Andrew <65128664+ahiser24@users.noreply.github.com>
2023-08-18 09:42:30 -0400

    Update Bulk Add Members to Meeting Policy.ps1

--- Teams/Bulk Add Members to Meeting Policy.ps1
+++ Teams/Bulk Add Members to Meeting Policy.ps1
@@ -2,7 +2,7 @@
 Connect-MicrosoftTeams
 
 #Get list of users
-$users = Get-Content -Path "C:\users\ahiser\Downloads\meeting_policy_bulk.txt"
+$users = Get-Content -Path "C:\users\USER\Downloads\meeting_policy_bulk.txt"
 
 #Grant specific Teams Meeting Policy. Change Policy Name to fit.
 Import-Csv -Path "$env:USERPROFILE\Downloads\premium.txt" | foreach{Grant-CsTeamsMeetingPolicy -Identity $_.email -PolicyName "Teams Premium Pilot"}
