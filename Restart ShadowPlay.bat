3a33b94a8e699bc1607e9c511927dd7ca29ed4af
Andrew <65128664+ahiser24@users.noreply.github.com>
2024-05-22 08:42:49 -0400

    Delete Restart ShadowPlay.bat

--- Restart ShadowPlay.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-net stop NVDisplay.ContainerLocalSystem
-net stop NvContainerLocalSystem
-net stop NvContainerNetworkService
-net start NVDisplay.ContainerLocalSystem
-net start NvContainerLocalSystem
-net start NvContainerNetworkService
